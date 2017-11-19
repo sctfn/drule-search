6bbd452439441a1423082e7c859fcc2a5540fca6
Scott Fenton <sctfen@gmail.com>
2017-11-19 15:02:23 -0500

    Added from leiningen

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to drule-search
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/drule_search/core_test.clj
@@ -0,0 +1,7 @@
+(ns drule-search.core-test
+  (:require [clojure.test :refer :all]
+            [drule-search.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

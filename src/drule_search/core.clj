f48d405ca3a3a62740d6a20daac89c5e020f6506
Scott Fenton <sctfen@gmail.com>
2017-11-19 15:24:04 -0500

    wrote some basic wff manipulators

--- src/drule_search/core.clj
+++ src/drule_search/core.clj
@@ -1,6 +1,26 @@
 (ns drule-search.core
   (:gen-class))
 
+(defn map-wff [f wff]
+  "Apply f to all variables in wff"
+  (def mw [w]
+    (if (number? w)
+      (f w)
+      (cons (first w) (map mw (rest w)))))
+  (mw wff))
+
+(defn maxvar [wff]
+  "Find the maximum variable number used in a wff"
+  (if (number? wff) wff (apply max (map maxvar (rest wff)))))
+
+(defn boostvar [wff n]
+  "Boost all variables used in wff by n"
+  (map-wff (fn [w] (+ w n)) wff))
+
+(defn make-subst [wff subs]
+  "Apply the substitutions in subs to wff"
+  (map-wff (fn [w] (get subs w w)) wff))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
